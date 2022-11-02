02940e46a7acaf4a6e5f2d648dbbb00dddacf445
Matthias Eckstaedt <matthias.eckstaedt@sap.com>
2022-11-02 15:30:34 +0100

    minot fix

--- next.config.js
+++ next.config.js
@@ -5,3 +5,12 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+}
