7160c1aa4df4cd8ecb5ad6d38d6cfdb28da3d24d
Matthias Eckstaedt <matthias.eckstaedt@sap.com>
2022-11-16 00:06:25 +0100

    add check in check 2

--- pages/api/checkInCheck.ts
+++ pages/api/checkInCheck.ts
@@ -8,7 +8,11 @@ export default async function handler(
     const { env } = req.body;
 
     try {
-        await CheckInUtils.executeCheckInCompletion(Number(10), env);
+        const attendances: any[] = await CheckInUtils.getOpenAttendances(env);
+
+        for (const att of attendances) {
+            await CheckInUtils.executeCheckInCompletion(att.id, env);
+        }
         res.status(200).end();
     } catch (error) {
         res.status(500).json(error)
--- pages/api/utils/CheckInUtils.ts
+++ pages/api/utils/CheckInUtils.ts
@@ -1,6 +1,28 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import dayjs from "dayjs";
 
 export class CheckInUtils {
+    public static async getOpenAttendances(env: string): Promise<any[]> {
+        let supabase: SupabaseClient;
+
+        if (env === "prod") {
+            supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL_PROD as string, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY_PROD as string);
+        } else {
+            supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string);
+        }
+
+        const { data, error } = await supabase
+            .from("attendances")
+            .select()
+            .lt("checkInActive", dayjs().subtract(1, "minute").toISOString());
+
+        if (error) {
+            throw error;
+        }
+
+        return data ?? [];
+    }
+
     public static async executeCheckInCompletion(id: number, env: string) {
         let supabase: SupabaseClient;
 
@@ -13,7 +35,7 @@ export class CheckInUtils {
         const { data: attData, error: attError } = await supabase
             .from("attendances")
             .update({
-                checkInActive: false,
+                checkInActive: false
             })
             .match({ id })
             .select()
@@ -43,6 +65,7 @@ export class CheckInUtils {
             .from("attendances")
             .update({
                 absentUsers,
+                checkInCompleted: true
             })
             .match({ id })
             .select();
