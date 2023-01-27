a87ff22df3b086d6804bc036dd09ef8bfeffb930
Matthias Eckstaedt <matthias.eckstaedt@sap.com>
2023-01-27 09:28:31 +0100

    sendPracticePlan

---
+++ pages/api/sendPracticePlan.ts
@@ -0,0 +1,29 @@
+import { Telegraf } from 'telegraf';
+
+const allowCors = (fn: any) => async (req: any, res: any) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    return await fn(req, res)
+}
+
+const handler = async (req: any, res: any) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    const { name, telegramId, url } = req.body; // appName
+
+    const telegraf = new Telegraf(process.env.VOS_BOT as string);
+
+    telegraf.telegram.sendDocument(telegramId, url);
+
+    res.status(200).end(JSON.stringify({ success: true }));
+}
+
+module.exports = allowCors(handler);
