9227cf171842daf1d196d064df256c300b642741
Matthias Eckstaedt <matthias.eckstaedt@sap.com>
2023-11-23 08:42:18 +0100

    add trips to remove user

--- pages/api/deleteUserFromAuth.ts
+++ pages/api/deleteUserFromAuth.ts
@@ -17,19 +17,23 @@ const allowCors = (fn: any) => async (req: any, res: any) => {
 
 const handler = async (req: any, res: any) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    const { appShortName, id } = req.body;
+    const { appShortName, id, useTrips } = req.body;
     let supabase: SupabaseClient;
 
-    if (appShortName === "SoS") {
-        supabase = createClient(process.env.NEXT_PUBLIC_ATT_URL as string, process.env.NEXT_PUBLIC_SUPABASE_ATT_SERVICE_ROLE_SOS as string);
-    } else if (appShortName === "VoS") {
-        supabase = createClient(process.env.NEXT_PUBLIC_ATT_URL_VOS as string, process.env.NEXT_PUBLIC_SUPABASE_ATT_SERVICE_ROLE_VOS as string);
-    } else if (appShortName === "BoS") {
-        supabase = createClient(process.env.NEXT_PUBLIC_ATT_URL_BOS as string, process.env.NEXT_PUBLIC_SUPABASE_ATT_SERVICE_ROLE_BOS as string);
-    } else if (appShortName === "GoS") {
-        supabase = createClient(process.env.NEXT_PUBLIC_ATT_URL_GOS as string, process.env.NEXT_PUBLIC_SUPABASE_ATT_SERVICE_ROLE_GOS as string);
+    if (useTrips) {
+        supabase = createClient(process.env.TRIPS_SUPABASE_URL as string, process.env.TRIPS_SUPABASE_SERVICE_ROLE as string);
     } else {
-        supabase = createClient(process.env.NEXT_PUBLIC_ATT_URL_JUCHO as string, process.env.NEXT_PUBLIC_SUPABASE_ATT_SERVICE_ROLE_JUCHO as string);
+        if (appShortName === "SoS") {
+            supabase = createClient(process.env.NEXT_PUBLIC_ATT_URL as string, process.env.NEXT_PUBLIC_SUPABASE_ATT_SERVICE_ROLE_SOS as string);
+        } else if (appShortName === "VoS") {
+            supabase = createClient(process.env.NEXT_PUBLIC_ATT_URL_VOS as string, process.env.NEXT_PUBLIC_SUPABASE_ATT_SERVICE_ROLE_VOS as string);
+        } else if (appShortName === "BoS") {
+            supabase = createClient(process.env.NEXT_PUBLIC_ATT_URL_BOS as string, process.env.NEXT_PUBLIC_SUPABASE_ATT_SERVICE_ROLE_BOS as string);
+        } else if (appShortName === "GoS") {
+            supabase = createClient(process.env.NEXT_PUBLIC_ATT_URL_GOS as string, process.env.NEXT_PUBLIC_SUPABASE_ATT_SERVICE_ROLE_GOS as string);
+        } else {
+            supabase = createClient(process.env.NEXT_PUBLIC_ATT_URL_JUCHO as string, process.env.NEXT_PUBLIC_SUPABASE_ATT_SERVICE_ROLE_JUCHO as string);
+        }
     }
 
     const { error: errorDeleteUser } = await supabase.auth.admin.deleteUser(id);
