f283f34cc5eca11cc5012998036dfdf3e93ed4ca
Matthias Eckstaedt <eckstaedt98@gmail.com>
2025-10-14 14:21:39 +0200

    adjust bg for attendix mail

--- pages/api/informAttendixUser.tsx
+++ pages/api/informAttendixUser.tsx
@@ -25,7 +25,7 @@ const handler = async (req: any, res: any) => {
         name,
         url: "https://attendix.de",
         appName: "Attendix",
-        imageUrl: "https://ultyjzgwejpehfjuyenr.supabase.co/storage/v1/object/public/profiles/login.jpeg",
+        imageUrl: "https://ultyjzgwejpehfjuyenr.supabase.co/storage/v1/object/public/profiles/bg.jpeg",
         tenant,
         role
     };
